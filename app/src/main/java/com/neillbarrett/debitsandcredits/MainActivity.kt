498eb77a6503be683a5444f7b1efafc9f288963c
NCBarrett <80253863+NCBarrett@users.noreply.github.com>
2023-02-01 12:17:10 -0800

    Added RecyclerView and Button code for Recipients fragment

--- app/src/main/java/com/neillbarrett/debitsandcredits/MainActivity.kt
+++ app/src/main/java/com/neillbarrett/debitsandcredits/MainActivity.kt
@@ -2,8 +2,13 @@ package com.neillbarrett.debitsandcredits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +24,9 @@ class MainActivity : AppCompatActivity() {
             .commitNow()
     }*/
 
+    private fun setupNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainFragment) as NavHostFragment
 
+        navController = navHostFragment.navController
+    }
 }
---
+++ app/src/main/java/com/neillbarrett/debitsandcredits/ManageRecipients/RecipientsViewModel.kt
@@ -0,0 +1,4 @@
+package com.neillbarrett.debitsandcredits.ManageRecipients
+
+class RecipientsViewModel {
+}
